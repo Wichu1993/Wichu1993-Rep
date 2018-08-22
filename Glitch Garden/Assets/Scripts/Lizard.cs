f4518b6a2dfaa0205ac8b920b7982ddfab380207
Wichu1993 <wisi1193@gmail.com>
2018-08-22 20:27:42 +0200

    Added Project

---
+++ Glitch Garden/Assets/Scripts/Lizard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour {
+
+    public Attacker attacker;
+
+	void Start () {
+        attacker = GetComponent<Attacker>();
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Glitch Garden/Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        float volume = PlayerPrefsManager.GetMasterVolume();
+        musicManager.SetVolume(volume);
+    }
+	
+}
