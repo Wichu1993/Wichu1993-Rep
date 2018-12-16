9d5713156ba557bbb6ddf0727b326e95772a570b
Wichu1993 <wisi1193@gmail.com>
2018-12-16 16:42:27 +0100

    1

---
+++ Hello World/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private string _Name = "Marcin";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello "+ _Name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
